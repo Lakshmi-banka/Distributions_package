f4ace1f5d84ae3a775b784b5bfbb7b5ef840dc6e
lakshmi banka <lakshmibanka@gmail.com>
2024-11-14 15:37:41 +0530

    files added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="distributions", 
+ version="0.1",
+ description="Gaussian distributions", 
+ packages=['distributions'],
+ author='Lakshmi Banka',
+ author_email='lakshmibanka516@gmail.com',
+ zip_safe=False)
